5bccd225e9f5430ccb0eedf02a95007685dec93f
shazin <shazin.sadakath@gmail.com>
2024-04-19 19:09:51 -0400

    Adding data get endpoint

--- pg-choreo-backend/src/main/java/com/medium/shazin/pg/choreo/backend/controller/DataController.java
+++ pg-choreo-backend/src/main/java/com/medium/shazin/pg/choreo/backend/controller/DataController.java
@@ -1,9 +1,15 @@
 package com.medium.shazin.pg.choreo.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/data")
 public class DataController {
 
-
+    @GetMapping
+    public String get() {
+        return "data is ready";
+    }
 }
