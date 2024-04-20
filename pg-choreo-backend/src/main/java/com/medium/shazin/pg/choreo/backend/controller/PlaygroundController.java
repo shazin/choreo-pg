11e223767712d62f5d8d78bc20a25260072e2b32
shazin <shazin.sadakath@gmail.com>
2024-04-19 20:50:54 -0400

    Adding PG controller

--- pg-choreo-backend/src/main/java/com/medium/shazin/pg/choreo/backend/controller/PlaygroundController.java
+++ pg-choreo-backend/src/main/java/com/medium/shazin/pg/choreo/backend/controller/PlaygroundController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/data")
-public class DataController {
+@RequestMapping("/playground")
+public class PlaygroundController {
 
     @GetMapping
     public String get() {
